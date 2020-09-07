beebf055796aa4b5bb626d32fb1fcd59a2786ac0
Nikhil Porwal <porwalnikhil7@gmail.com>
2020-09-07 13:21:39 +0530

    Update Anon.cs
    fixed build failure

--- BAL/Anon.cs
+++ BAL/Anon.cs
@@ -11,5 +11,11 @@ namespace BAL
         public string EMP_UAN { get; set; }
         public string EMP_NAME { get; set; }
         public int EMP_WAGE { get; set; }
+        public int EMP_WAGE2 { get; set; }
+        public int EMP_WAGE3 { get; set; }
+        public int EMP_WAGE4 { get; set; }
+        public int EE_Percent { get; set; }
+        public int ER_Percent { get; set; }
+        public int PF { get; set; }
     }
 }
