8f4db82e57358d7daab0ebc405cce4447a8ff298
Nikhil Porwal <nikhil_porwal@live.com>
2024-04-04 23:09:57 +0530

    #8, Upload Feature initial working code | reviewNot Done

--- EPFODataLoader_WebForm/Contact.aspx
+++ EPFODataLoader_WebForm/Contact.aspx
@@ -3,15 +3,25 @@
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
     <h2><%: Title %>.</h2>
     <h3>Your contact page.</h3>
-    <address>
-        One Microsoft Way<br />
-        Redmond, WA 98052-6399<br />
-        <abbr title="Phone">P:</abbr>
-        425.555.0100
-    </address>
+   
+    <div class="jumbotron" style="overflow: scroll">
+        <asp:checkbox id="chkToggleHigherWages" type="checkbox" Text="Limit Wages to 15K" runat="server" />
+        <h1>Upload CSV data</h1>
+        
+        <asp:FileUpload ID="CSVFileUpload_Id" runat="server" Width="138px" />
 
-    <address>
-        <strong>Support:</strong>   <a href="mailto:Support@example.com">porwalprofessionalconsultancy@gmail.com</a><br />
-        <strong>Marketing:</strong> <a href="mailto:Marketing@example.com">Marketing@example.com</a>
-    </address>
+        <h1>Download processed data</h1>
+        <asp:Button ID="Button1" runat="server" Text="Download Txt File" OnClick="DownloadTxtData" Width="200px"/>
+    </div>
+
+     <address>
+     One Microsoft Way<br />
+     Redmond, WA 98052-6399<br />
+     <abbr title="Phone">P:</abbr>
+     425.555.0100
+ </address>
+ <address>
+     <strong>Support:</strong>   <a href="mailto:Support@example.com">porwalprofessionalconsultancy@gmail.com</a><br />
+     <strong>Marketing:</strong> <a href="mailto:Marketing@example.com">Marketing@example.com</a>
+ </address>
 </asp:Content>
--- EPFODataLoader_WebForm/Contact.aspx.cs
+++ EPFODataLoader_WebForm/Contact.aspx.cs
@@ -1,17 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Data;
+using System.IO;
 using System.Web.UI;
-using System.Web.UI.WebControls;
+using BAL;
 
 namespace EPFODataLoader_WebForm
 {
     public partial class Contact : Page
     {
+        bool isHigherWageLimited = false;
         protected void Page_Load(object sender, EventArgs e)
         {
+            isHigherWageLimited = chkToggleHigherWages.Checked;
+        }
 
+        protected void DownloadTxtData(System.Object sender, System.EventArgs e)
+        {
+            string serverStorageLocatn = "~/MyFolder/";
+
+            if (CSVFileUpload_Id.HasFile)
+            {
+                string ext = Path.GetExtension(CSVFileUpload_Id.FileName).ToLower();
+                var fileName = CSVFileUpload_Id.FileName;
+
+                //getting the path of the file   
+                CSVFileUpload_Id.SaveAs(Server.MapPath(serverStorageLocatn + CSVFileUpload_Id.FileName));
+                string path = Server.MapPath(serverStorageLocatn + CSVFileUpload_Id.FileName);
+
+                DataTable dt = new DataTable();
+                dt = GetDT(path);
+
+                //set data in dataTable before passing in Utilities
+                var result2 = ExcelUtilities.ReadCSVFile(dt, isHigherWageLimited);
+
+                //DownloadTxt(fileName);
+                //var result = ExcelUtilities.DumpDBFToTxt(dt).ToString();
+                string txtFilePath = "_" + fileName.Replace("csv", "txt");
+                Response.Clear();
+                Response.AddHeader("content-disposition", "attachment; filename=" + txtFilePath);
+                Response.AddHeader("content-type", "text/plain");
+
+                using (StreamWriter writer = new StreamWriter(Response.OutputStream))
+                {
+                    writer.Write(result2);
+                }
+                Response.End();
+            }
+            else
+            {
+                //Label1.Text = "No File Uploaded.";
+            }
+        }
+
+        private DataTable GetDT(string fileName)
+        {
+            string[] Lines = File.ReadAllLines(fileName);
+            string[] Fields;
+            Fields = Lines[0].Split(new char[] { ',' });
+            int Cols = Fields.GetLength(0);
+            DataTable dt = new DataTable();
+            //1st row must be column names; force lower case to ensure matching later on.
+            for (int i = 0; i < Cols; i++)
+                dt.Columns.Add(Fields[i].ToLower(), typeof(string));
+            DataRow Row;
+            for (int i = 1; i < Lines.GetLength(0); i++)
+            {
+                Fields = Lines[i].Split(new char[] { ',' });
+                Row = dt.NewRow();
+                for (int f = 0; f < Cols; f++)
+                    Row[f] = Fields[f];
+                dt.Rows.Add(Row);
+            }
+            return dt;
         }
     }
 }
