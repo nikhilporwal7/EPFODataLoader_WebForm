6ae1839483f619e29504cf92db1e182a3e981587
Nikhil Porwal <porwalnikhil7@gmail.com>
2025-07-08 06:28:04 +0530

    #34, Make UANs transparent for user to see and Add.

---
+++ EPFODataLoader_WebForm/UAN_Viewer.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="UAN Excluded List" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="UAN_Viewer.aspx.cs" Inherits="EPFODataLoader_WebForm.UAN_Viewer" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h3>Your UAN Page for EPS wage exclusion. UAN should be part of this list if you're planning to use the age 58 and above checkbox<i>age 58 and above checkbox</i> 
+        for fixing the wages</h3>
+   
+    <div class="jumbotron" style="overflow: scroll">
+        <asp:Literal ID="literalMsg" runat="server"></asp:Literal> <br />
+    </div>
+
+   <div class="jumbotron" style="overflow: scroll">
+        <h1>Add more UANs to List</h1>
+        <asp:TextBox ID="txtBoxInput" runat="server" Width="200px"></asp:TextBox>
+        <asp:Button ID="btnAddUANs" runat="server" Text="Add UAN's" OnClick="AddUANs" Width="200px"/>
+    </div>
+
+
+</asp:Content>
---
+++ EPFODataLoader_WebForm/UAN_Viewer.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EPFODataLoader_WebForm
+{
+    public partial class UAN_Viewer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string filePath = Server.MapPath("~/MyFolder/UAN_EPSExcluded"); // Or your file path
+                                                                                //string UAN_EPS_Excluded_File_Name = "UAN_EPSExcluded";
+                if (File.Exists(filePath))
+                {
+                    string fileContent = File.ReadAllText(filePath);
+
+                    literalMsg.Text = fileContent.Replace(Environment.NewLine, "<br/>"); // Or Label1.Text
+                }
+                else
+                {
+                    // File does not exist, handle the case
+                    Response.Write("File does not exist.");
+                }
+            }
+        }
+
+        protected void AddUANs(object sender, EventArgs e)
+        {
+            string filePath = Server.MapPath("~/MyFolder/UAN_EPSExcluded");
+            File.AppendAllText(filePath, txtBoxInput.Text + Environment.NewLine);
+            string fileContent = File.ReadAllText(filePath);
+
+            literalMsg.Text = fileContent.Replace(Environment.NewLine, "<br/>"); // Or Label1.Text
+            //literalMsg.Text = txtBoxInput.Text.Replace(Environment.NewLine, "<br/>"); 
+        }
+    }
+}
